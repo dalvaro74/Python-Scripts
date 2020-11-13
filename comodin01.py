ecd609d7710f82909ceae82f6c97a4fc118f84ab
dalvaro <dalvaro74@gmail.com>
2020-11-13 18:07:34 +0100

    Cambio comodin y añadimos notebook

--- comodin01.py
+++ comodin01.py
@@ -1,15 +1,7 @@
 import numpy as np
 import pandas as pd
-from sklearn.preprocessing import PolynomialFeatures
-X = np.arange(6).reshape(3, 2)
-X
-
-
-
-poly = PolynomialFeatures(2)
-poly.fit_transform(X)
-
-
-
-poly = PolynomialFeatures(interaction_only=True)
-poly.fit_transform(X)
+import matplotlib.pyplot as plt
+import statsmodels.api as sm
+# We can override the default matplotlib styles with those of Seaborn
+import seaborn as sns
+sns.set()
